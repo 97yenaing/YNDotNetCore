27c7ed88562cabb32105518c4a6a86b5ceec401f
MAM <MAM@DESKTOP-CENKFK6>
2024-04-13 13:44:03 +0630

    Add project files.

---
+++ YNDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-CENKFK6";
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "@naing?007bl@ck#";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection Open");
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection Close");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id =>" + dr["BlogId"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("--------------------------------------");
+}
+
+Console.ReadKey();
