f55cbbe98d0d743e06b857e1309aebf57091805d
YeNaing <yenaingucsy@gmail.com>
2024-04-14 09:28:15 +0630

    [Complete]Ado.Net CRUD

--- YNDotNetCore.ConsoleApp/Program.cs
+++ YNDotNetCore.ConsoleApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using YNDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+/*SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
 stringBuilder.DataSource = "DESKTOP-CENKFK6";
 stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
 stringBuilder.UserID = "sa";
@@ -28,6 +29,12 @@ foreach (DataRow dr in dt.Rows)
     Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
     Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
     Console.WriteLine("--------------------------------------");
-}
-
+}*/
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+/*adoDotNetExample.Read();*/
+/*adoDotNetExample.Create("title", "author", "content");*/
+/*adoDotNetExample.Update(12, "test title", "test author", "test content");*/
+/*adoDotNetExample.Delete(13);*/
+adoDotNetExample.Edit(13);
+adoDotNetExample.Edit(5);
 Console.ReadKey();
