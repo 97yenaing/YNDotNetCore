5861534559ad96645eb95337517907992474f16d
YeNaing <yenaingucsy@gmail.com>
2024-04-27 17:20:46 +0630

    2 day complete

--- YNDotNetCore.ConsoleApp/EFCoreExample.cs
+++ YNDotNetCore.ConsoleApp/EFCoreExample.cs
@@ -12,8 +12,11 @@ namespace YNDotNetCore.ConsoleApp
         public void Run()
         {
             /*Read();*/
-            Edit(3);
-            Edit(50);
+            /*Edit(3);
+            Edit(50);*/
+            /*Create("EfTitle", "EfAuthor", "EfContent");*/
+            /* Update(6,"EfTitleUpdate", "EfAuthorUpdate", "EfContentUpdate");*/
+            Delete(6);
         }
         private void Read()
         {
@@ -44,5 +47,58 @@ namespace YNDotNetCore.ConsoleApp
             Console.WriteLine(item.BlogContent);
             Console.WriteLine("--------------------------------");
         }
-    }
+
+        private void Create(string title,string author,string content) {
+            var item = new BlogDto
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+            };
+            db.Blogs.Add(item);
+            int result=db.SaveChanges();//Query EXecute EfCore
+
+            string message = result > 0 ? "saving Successful." : "Saving Failed";
+            Console.WriteLine(message);
+        }
+
+        private void Update(int id, String title, string author, string content)
+        {
+            var item = db.Blogs.FirstOrDefault(x => x.BlogId == id); // already get real db state 
+            if (item is null)
+            {
+                Console.WriteLine("No Data Found");
+                return;
+            }
+            item.BlogTitle = title; // already prepare update value 
+            item.BlogAuthor = author;
+            item.BlogContent = content;
+
+            int result = db.SaveChanges();//Query EXecute EfCore
+
+            string message = result > 0 ? "Updating  Successful." : "Updating Failed";
+            Console.WriteLine(message);
+
+        }
+        private void Delete(int id)
+        {
+            var item = db.Blogs.FirstOrDefault(x => x.BlogId == id); // already get real db state 
+            if (item is null)
+            {
+                Console.WriteLine("No Data Found");
+                return;
+            }
+            db.Blogs.Remove(item);
+            int result = db.SaveChanges();//Query EXecute EfCore
+
+            string message = result > 0 ? "Deleting  Successful." : "Deleting Failed";
+            Console.WriteLine(message);
+        }
+
+        // for database to C# code changes //efcore command database first serach in google
+
+
+
+
+    }   
 }
